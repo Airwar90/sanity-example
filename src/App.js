b1fb7abfa822bb26c566f29ae73deea6c64440da
Marta Grazia <airwar90@gmail.com>
2025-11-30 18:35:20 +0000

    converted browser router with hashrouter for github pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route,  Routes, Link } from "react-router-dom";
-import Header from "./components/Header";
-import HomePage from "./pages/HomePage";
-import Blog from "./pages/Blog";
-import SingleBlogPage from "./pages/SingleBlogPage";
-import ErrorPage from "./pages/ErrorPage";
+import { HashRouter, Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import HomePage from "./pages/HomePage"
+import Blog from "./pages/Blog"
+import SingleBlogPage from "./pages/SingleBlogPage"
+import ErrorPage from "./pages/ErrorPage"
 
 function App() {
   return (
-    <BrowserRouter> 
-     <Header/>
+    <HashRouter>
+      <Header />
       <Routes>
-        <Route exact path="/" element={<HomePage/>}></Route>
-        <Route path="/blog" element={<Blog/>}></Route>
-        <Route path="/blog/:slug" element={<SingleBlogPage/>}></Route>
-        <Route path="*" element={<ErrorPage/>}></Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<SingleBlogPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
